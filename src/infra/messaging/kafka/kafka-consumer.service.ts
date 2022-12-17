39fa7d761b7321175edbe819ab2ed0485eeb3f24
Kauacnok <kauaprofessionalcontact@gmail.com>
2022-12-17 16:35:51 -0300

    Added env.example and renamed environment variable name

--- src/infra/messaging/kafka/kafka-consumer.service.ts
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -11,7 +11,7 @@ export class KafkaConsumerService extends ServerKafka implements OnModuleDestroy
 				sasl: {
 					mechanism: 'scram-sha-256',
 					username: 'd2lzZS1iZWFyLTUwNDQkUj7s3h2ONMtk7l_bIN_M26pO7AE811DAX9Kwi_HnIBs',
-					password: process.env.PASSWORD || '',
+					password: process.env.KAFKA_PASSWORD || '',
 				},
 
 				ssl: true,
